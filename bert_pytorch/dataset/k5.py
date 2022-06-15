11ba366f54081787ea5ad595ca40b9645a00a63d
code <code@gmail.com>
2022-06-15 15:56:59 +0800

    增加测试数据集

--- bert_pytorch/dataset/k5.py
+++ bert_pytorch/dataset/k5.py
@@ -4,8 +4,10 @@ import pandas as pd
 datak5=pd.read_csv('nas.csv')
 data_train=[]
 data_y=[]
+data_test=[]
 
 for i in range(len(datak5)-6):
     data_train.append(datak5.iloc[i:i+5])
     data_y.append(datak5.iloc[i+6])
+    data_test.append(datak5.iloc[i+1:i+6])
 
