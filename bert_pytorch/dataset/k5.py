796c012fe58bc8b7c6fdb65ef196567339579a1c
code <code@gmail.com>
2022-06-15 15:55:43 +0800

    修改数据集名称

--- bert_pytorch/dataset/k5.py
+++ bert_pytorch/dataset/k5.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 
 datak5=pd.read_csv('nas.csv')
-data_train=[]
-data_y=[]
+data_train_x=[]
+data_train_y=[]
 
 for i in range(len(datak5)-6):
-    data_train.append(datak5.iloc[i:i+5])
-    data_y.append(datak5.iloc[i+6])
+    data_train_x.append(datak5.iloc[i:i+5])
+    data_train_y.append(datak5.iloc[i+6])
 
