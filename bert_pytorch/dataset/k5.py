964ee108dd525cda9a5e81326cf897e563881442
code <code@gmail.com>
2022-06-15 15:53:32 +0800

    制作数据集

--- bert_pytorch/dataset/k5.py
+++ bert_pytorch/dataset/k5.py
@@ -2,5 +2,10 @@ import numpy as np
 import pandas as pd
 
 datak5=pd.read_csv('nas.csv')
-print(datak5.head)
+data_train=[]
+data_y=[]
+
+for i in range(len(datak5)-6):
+    data_train.append(datak5.iloc[i:i+5])
+    data_y.append(datak5.iloc[i+6])
 
